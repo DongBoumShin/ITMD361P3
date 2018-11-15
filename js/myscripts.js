2ca58f67350af6cb6b958cc0d57f9c15685c9075
DongBoumShin <42655143+DongBoumShin@users.noreply.github.com>
2018-11-14 22:36:32 -0600

    Added starting point and custom marker!

--- js/myscripts.js
+++ js/myscripts.js
@@ -3,6 +3,10 @@ function initMap() {
   var keeper = {lat: 33.774786, lng: -117.866527};
   // The map, centered at the Greed
   var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: keeper});
+  var greed = {
+    url: "../ext_content/greedmarker.jpg",
+    scaledSize: new google.maps.Size(64,64)
+  }
   // The marker, positioned at the Greed
   var marker = new google.maps.Marker({
     position: keeper,
