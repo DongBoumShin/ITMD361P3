01f1264547df5c9a7f68e48423f55d432590479f
DongBoumShin <42655143+DongBoumShin@users.noreply.github.com>
2018-11-20 19:35:59 -0600

    I got the icon on the map to bounce.
    also renamed js for clarity. Trying to toggle bounce but can't.

--- js/mymapscripts.js
+++ js/mymapscripts.js
@@ -1,19 +1,27 @@
+var marker;
 function initMap() {
   // The location of the Greed
   var keeper = {lat: 33.774786, lng: -117.866527};
   // The map, centered at the Greed
   var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: keeper});
-  var greed = {
-    url: "../ext_content/greedmarker.jpg",
-    scaledSize: new google.maps.Size(64,64)
-  }
+  var greed = "../ext_content/greedmarker.jpg";
   // The marker, positioned at the Greed
-  var marker = new google.maps.Marker({
+  marker = new google.maps.Marker({
     position: keeper,
     map: map,
-    icon: greed
+    //icon: greed,
+    animation: google.maps.Animation.BOUNCE,
+    title: "Afterbirth!"
   });
+  marker.addEventListener("click", toggleBounce);
 }
+  function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  }
 
 function rotate(speed) {
   var looper;
