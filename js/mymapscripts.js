11bc7f5ef9c5e6c13203c8eb5194a69befb75ab0
DongBoumShin <42655143+DongBoumShin@users.noreply.github.com>
2018-11-20 19:39:21 -0600

    changed the 'greed' to an image
    according to the google tutorial, but still won't work.

--- js/mymapscripts.js
+++ js/mymapscripts.js
@@ -4,7 +4,7 @@ function initMap() {
   var keeper = {lat: 33.774786, lng: -117.866527};
   // The map, centered at the Greed
   var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: keeper});
-  var greed = "../ext_content/greedmarker.jpg";
+  var greed = '../ext_content/greedmarker.jpg'
   // The marker, positioned at the Greed
   marker = new google.maps.Marker({
     position: keeper,
